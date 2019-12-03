6c16ec63ea1766a50a03b658c07198d53ddca748
Btforce <tashirobradley@gmail.com>
2019-12-03 13:14:18 -0800

    Merge branch 'master' of /Users/per6/AndroidStudioProjects/ChampionSpotlight with conflicts.

---
+++ app/src/main/java/com/example/championspotlight/QFragment.java
@@ -0,0 +1,14 @@
+package com.example.championspotlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qfragment);
+    }
+}
