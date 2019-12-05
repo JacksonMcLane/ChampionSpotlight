4519582dce98268bf632b42088f9d7d1f0585967
Btforce <tashirobradley@gmail.com>
2019-12-05 13:05:25 -0800

    Working on E fragment

--- app/src/main/java/com/example/championspotlight/QFragment.java
+++ app/src/main/java/com/example/championspotlight/QFragment.java
@@ -1,14 +1,52 @@
 package com.example.championspotlight;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class QFragment extends AppCompatActivity {
+public class QFragment extends Fragment {
 
+    TextView textViewName, textViewCost, textViewRange, textViewCostNum, textViewRangeNum, textViewDescription;
+    ImageView imageViewPicture;
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qfragment);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_passive_fragment, container, false);
+        wireWidgets(rootView);
+
+
+        textViewName.setText("Urchin Strike");
+
+        textViewRangeNum.setText("550");
+
+        textViewCostNum.setText("50 Mana");
+
+        textViewDescription.setText("Fizz dashes through his target, dealing (+100% total)" +
+                " physical damage plus magic damage. This spell applies on-hit effects.");
+
+
+
+
+        return rootView;
+    }
+
+    private void wireWidgets(View rootView) {
+        textViewName = rootView.findViewById(R.id.textView_qFragment_name);
+        textViewCost = rootView.findViewById(R.id.textView_qFragment_cost);
+        textViewRange = rootView.findViewById(R.id.textView_qFragment_range);
+        textViewCostNum = rootView.findViewById(R.id.textView_qFragment_costNumber);
+        textViewRangeNum = rootView.findViewById(R.id.textView_qFragment_rangeNumber);
+        imageViewPicture = rootView.findViewById(R.id.imageView_qFragment_image);
+        textViewDescription = rootView.findViewById(R.id.textView_qFragment_description);
     }
 }
