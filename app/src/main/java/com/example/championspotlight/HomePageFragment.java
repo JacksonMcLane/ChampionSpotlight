1426136c115dc501b434b4759ddd7271655a829e
Btforce <tashirobradley@gmail.com>
2019-12-11 13:59:44 -0800

    finished homefragment xml

--- app/src/main/java/com/example/championspotlight/HomePageFragment.java
+++ app/src/main/java/com/example/championspotlight/HomePageFragment.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class HomePageFragment extends Fragment {
-
+    TextView textViewWelcome, textViewName, textViewTitle;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
