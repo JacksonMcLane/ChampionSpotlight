ca3928aa4c5d93230267ca0a3cb64b566f5b8728
JacksonMcLane <jackson.mclane20@gmail.com>
2019-12-13 13:56:15 -0800

    Made things work

--- app/src/main/java/com/example/championspotlight/StatsSkinsFragment.java
+++ app/src/main/java/com/example/championspotlight/StatsSkinsFragment.java
@@ -2,7 +2,6 @@ package com.example.championspotlight;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
@@ -11,17 +10,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class StatsSkinsFragment extends Fragment {
 
-    private TextView textViewStats;
+    private TextView textViewStats, textViewStatsHeader;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_stats_skins_fragment, container, false);
-        textViewStats = rootView.findViewById(R.id.textView_stats_text);
+        wireWidgets(rootView);
         textViewStats.setText(
                 "Health: 570 – 2236    Attack Damage: 58.04 – 109.04\n" +
                 "Health Regen: 8 – 19.9    Attack Speed: 0.658 (+ 0 – 52.7%)\n" +
@@ -29,11 +26,12 @@ public class StatsSkinsFragment extends Fragment {
                 "Magic Resist: 32.1 – 53.35    Range: 175\n" +
                 "Mana: 317.2 – 946.2    Range Type: MELEE\n" +
                 "Mana Regen: 6 – 19.6    Move Speed: 335");
-        wireWidgets(rootView);
+        textViewStatsHeader.setText(R.string.fizz_base_stats);
         return rootView;
     }
 
     private void wireWidgets(View rootView) {
         textViewStats = rootView.findViewById(R.id.textView_stats_text);
+        textViewStatsHeader = rootView.findViewById(R.id.textView_stats_header);
     }
 }
