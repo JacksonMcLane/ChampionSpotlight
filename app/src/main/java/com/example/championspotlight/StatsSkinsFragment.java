51026e2cd9eb9d5b886818091374f4f642e14395
JacksonMcLane <jackson.mclane20@gmail.com>
2019-12-17 13:45:28 -0800

    Added Icon and completed carousel

--- app/src/main/java/com/example/championspotlight/StatsSkinsFragment.java
+++ app/src/main/java/com/example/championspotlight/StatsSkinsFragment.java
@@ -5,14 +5,25 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class StatsSkinsFragment extends Fragment {
 
     private TextView textViewStats, textViewStatsHeader;
+    private CarouselView carouselView;
+    private int[] skinsPhotos = {R.drawable.fizz_skin_default, R.drawable.fizz_skin_atlantean,
+            R.drawable.fizz_skin_tundra, R.drawable.fizz_skin_fisherman, R.drawable.fizz_skin_void,
+            R.drawable.fizz_skin_cottontail, R.drawable.fizz_skin_super_galaxy,
+            R.drawable.fizz_skin_omega_squad, R.drawable.fizz_skin_fuzz, R.drawable.fizz_skin_fuzz_prestige_edition};
 
     @Nullable
     @Override
@@ -27,11 +38,20 @@ public class StatsSkinsFragment extends Fragment {
                 "Mana: 317.2 – 946.2    Range Type: MELEE\n" +
                 "Mana Regen: 6 – 19.6    Move Speed: 335");
         textViewStatsHeader.setText(R.string.fizz_base_stats);
+        carouselView.setPageCount(skinsPhotos.length);
+        ImageListener imageListener = new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(skinsPhotos[position]);
+            }
+        };
+        carouselView.setImageListener(imageListener);
         return rootView;
     }
 
     private void wireWidgets(View rootView) {
         textViewStats = rootView.findViewById(R.id.textView_stats_text);
         textViewStatsHeader = rootView.findViewById(R.id.textView_stats_header);
+        carouselView = rootView.findViewById(R.id.carousel_view_stats_skins_view);
     }
 }
